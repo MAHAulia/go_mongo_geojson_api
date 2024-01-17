d98b52b244bf8ee254c17a2d06e478b0d262d748
Moh Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2024-01-17 18:00:35 +0700

    feat(route): install chi to handle incomming request - add base route - add health check route

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module geojosn-api
 
 go 1.21.3
+
+require github.com/go-chi/chi v1.5.5
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
 
 func main() {
 	fmt.Println("GeoJSON Server...")
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Selamat datang di server..."))
+	})
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Server is working..."))
+	})
+
+	fmt.Println("GeoJSON Server runnincleag on http://127.0.0.1:3333")
+	http.ListenAndServe(":3333", r)
+
 }
