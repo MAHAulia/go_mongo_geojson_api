a3a8d0a55dc8f4e75e91da3d0664d0a6ae27d622
Moh Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2024-01-18 12:11:58 +0700

    fix(port): fix port untuk deploy ke fly.io

--- main.go
+++ main.go
@@ -43,7 +43,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	fmt.Println("Initialize mongodb connection finish...")
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -55,13 +54,18 @@ func main() {
 	})
 
 	r.Get("/coffee_shops", getAllCofeeShop)
+	// r.Post("/coffee_shops", getAllCofeeShop)
+	// r.Post("/coffee_shops/near", getAllCofeeShop)
 
 	appPort := os.Getenv("APP_PORT")
 	if appPort == "" {
-		log.Fatal("Please set yout app PORT")
+		appPort = "9000"
 	}
-	fmt.Printf("GeoJSON Server runnincleag on http://127.0.0.1:%s\n\n", appPort)
-	http.ListenAndServe(":"+appPort, r)
+	fmt.Printf("GeoJSON Server runnincleag on http://0.0.0.0:%s\n\n", appPort)
+
+	// http.ListenAndServe("0.0.0.0:"+appPort, r)
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+appPort, r))
+	// log.Fatal(http.ListenAndServe(":"+appPort, r))
 
 }
 
@@ -93,3 +97,32 @@ func getAllCofeeShop(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(jsonData)
 }
+
+// func getCoffeeShopByName(w http.ResponseWriter, r *http.Request) {
+
+// 	coll := mongodb.Client.Database(os.Getenv("DB_NAME")).Collection("coffee_shop")
+
+// 	cursor, err := coll.Find(context.TODO(), bson.D{})
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer cursor.Close(context.TODO())
+
+// 	var results []bson.M
+// 	if err = cursor.All(context.TODO(), &results); err != nil {
+// 		panic(err)
+// 	}
+
+// 	if len(results) == 0 {
+// 		fmt.Printf("No documents were found\n")
+// 		return
+// 	}
+
+// 	jsonData, err := json.MarshalIndent(results, "", "    ")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.Write(jsonData)
+// }
